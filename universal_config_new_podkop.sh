b92431ea462fc66c856bc5cbdc8460a6cc6754fa
CodeRoK7 <58553260+CodeRoK7@users.noreply.github.com>
2025-10-01 17:36:13 +0000

    Fix url packets Podkop

--- universal_config_new_podkop.sh
+++ universal_config_new_podkop.sh
@@ -1116,7 +1116,7 @@ else
 		for file in $podkop_files
 		do
 			echo "Download $file..."
-			wget -q -O "$DOWNLOAD_DIR/$file" "$URL/podkop_packets/$file"
+			wget -q -O "$DOWNLOAD_DIR/$file" "$URL/packets/$file"
 		done
 		opkg install $DOWNLOAD_DIR/podkop*.ipk
 		opkg install $DOWNLOAD_DIR/luci-app-podkop*.ipk
